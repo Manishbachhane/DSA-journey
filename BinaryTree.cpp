0f8e0c7ffc938423d2e28c8155e44a9089ad97e9
Manishbachhane <manishbachhane@gmail.com>
2025-08-04 12:36:04 +0530

    BinaryTree add traversing function

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -7,23 +7,25 @@ struct node{
 };
 class BST{
     private:
-        node *root;      
+        node *root;   
+     protected:
+        void preorderrec(node*);
+        void inorderrec(node*);
+        void postorderrec(node*);   
     public:
         BST();
     bool isEmpty();
     void insert(int);
-    void preorder();
-    void inorder();
-    void postorder();
-}
+    void preorder();//traversing
+    void inorder();//traversing
+    void postorder();//traversing
+};
 BST::BST(){
     root=nullptr;
 }
-
 bool BST::isEmpty(){
     return root==nullptr;
 }
-
 void BST::insert(int data){
     node *ptr;
     node *n=new node;
@@ -63,3 +65,39 @@ void BST::preorder()
 {
     preorderrec(root);
 }
+void BST::preorderrec(node *ptr)
+{
+    if(ptr)
+    {
+        cout<<ptr->item<<" ";
+        preorderrec(ptr->left);
+        preorderrec(ptr->right);
+    }
+}
+void BST::inorder()
+{
+    inorderrec(root);
+}
+void BST::inorderrec(node *ptr)
+{
+    if(ptr)
+    {
+        inorderrec(ptr->left);
+        cout<<ptr->item<<" ";
+        inorderrec(ptr->right);
+    }
+}
+void BST::postorder()
+{
+    postorderrec(root);
+}
+void BST::postorderrec(node *ptr)
+{
+    if(ptr)
+    {
+        postorderrec(ptr->left);
+        postorderrec(ptr->right);
+        cout<<ptr->item<<" ";
+    }
+}
+
